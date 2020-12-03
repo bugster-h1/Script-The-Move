2792a9eb9acf89a4193bc1d8e63ec356513d942f
Volodymyr Romaniv <volodymyr.romaniv@eleks.com>
2020-12-03 17:39:50 +0200

    Improved pause finctionality

--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PauseController : MonoBehaviour
 {
-    public GameObject[] gameObjects;
+	public static Dictionary<int, bool> initialStates = new Dictionary<int, bool>();
 
-    public void TogglePause()
+    public GameObject[] objectsToDisableOnPause;
+	public GameObject pauseMenu;
+
+    public void Pause()
 	{
-		Time.timeScale = Time.timeScale == 1 ? 0 : 1;
+		Time.timeScale = 0;
+		initialStates = objectsToDisableOnPause.ToDictionary(x => x.GetHashCode(), x => x.activeSelf);
 
-		foreach (var item in gameObjects)
+		foreach (var item in objectsToDisableOnPause)
 		{
-			item.SetActive(!item.activeSelf);
+			item.SetActive(false);
 		}
+
+		pauseMenu.SetActive(true);
+	}
+
+	public void UnPause()
+	{
+		Time.timeScale = 1;
+
+		foreach (var item in objectsToDisableOnPause)
+		{
+			item.SetActive(initialStates[item.GetHashCode()]);
+		}
+
+		pauseMenu.SetActive(false);
 	}
 }
+
 
